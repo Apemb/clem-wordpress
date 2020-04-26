85d9ad849a1ed667fba22c2ad3692a776e5189a8
Antoine Boileau <antoine@deliver.ee>
2020-04-26 15:24:00 +0200

    Add Neve and Unvind theme

---
+++ web/app/themes/siteorigin-unwind/inc/contributors.php
@@ -0,0 +1,59 @@
+<?php
+return json_decode( '{
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 5436,
+		"score": 129.37871652662088,
+		"percent": 55.09934948403238
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 8290,
+		"score": 93.83170095033877,
+		"percent": 39.9607124119224
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 93,
+		"score": 6.265128345965725,
+		"percent": 2.668170666429931
+	},
+	"d1bea7e3f82e561c135bf5a3d4f9f896": {
+		"name": "SiteOrigin Support",
+		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
+		"loc": 187,
+		"score": 3.783182134193696,
+		"percent": 1.6111682057905963
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 36,
+		"score": 0.8557087020628967,
+		"percent": 0.3644261907775993
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 13,
+		"score": 0.5141948243076693,
+		"percent": 0.218983470295745
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 42,
+		"score": 0.11398581909380327,
+		"percent": 0.04854387685304388
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 7,
+		"score": 0.06726291953140207,
+		"percent": 0.028645693898304334
+	}
+}', true );
